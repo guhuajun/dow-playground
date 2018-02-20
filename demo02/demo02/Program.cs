be928eb2e8f7f91db51bee240786e9ac246b0c23
Greg Gu <contact@guhuajun.com>
2018-02-20 14:24:55 +0800

    add two demos

---
+++ demo02/demo02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace demo02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demo02: Hello World!");
+        }
+    }
+}
