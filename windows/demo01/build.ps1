f4f77a5b8a52824cfb05d61ea0d00568f97bd234
Greg Gu <contact@guhuajun.com>
2018-05-19 18:23:05 +0800

    add files

---
+++ windows/demo01/build.ps1
@@ -0,0 +1,1 @@
+docker build --rm --tag greggu/test:0.0.1 .
---
+++ windows/demo02/build.ps1
@@ -0,0 +1,2 @@
+docker build -f Dockerfile.1 --rm --tag greggu/demo01:0.0.1 .
+docker build -f Dockerfile.2 --rm --tag greggu/demo02:0.0.1 .
